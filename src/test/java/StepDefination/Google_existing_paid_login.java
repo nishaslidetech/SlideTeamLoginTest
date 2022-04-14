ab45cb99bc87a8ecc741ba71b431c8b618d46f0c
Nisha Dhiman <91591950+nishaslidetech@users.noreply.github.com>
2022-04-14 18:33:08 +0530

    Update Google_existing_paid_login.java

--- src/test/java/StepDefination/Google_existing_paid_login.java
+++ src/test/java/StepDefination/Google_existing_paid_login.java
@@ -82,7 +82,7 @@ public class Google_existing_paid_login extends BaseClass {
 			WebElement gmail_pass = wait.until(ExpectedConditions
 					.elementToBeClickable(By.cssSelector("#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input")));
 
-			gmail_pass.sendKeys("parulpahwa@11");
+			gmail_pass.sendKeys("parulpahwa@12");
 
 			WebElement next_2 = driver.findElement(By.cssSelector("#passwordNext > div > button > span"));
 
