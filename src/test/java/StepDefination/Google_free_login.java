75abb8fd7a824053854035fd7073ff7f653de55e
nishaslidetech <nisha.dhiman@slidetech.in>
2022-07-06 15:58:04 +0530

    update

--- src/test/java/StepDefination/Google_free_login.java
+++ src/test/java/StepDefination/Google_free_login.java
@@ -95,14 +95,14 @@ public class Google_free_login extends BaseClass {
 			}
 
 			WebElement gmail_email = BaseClass.elementToBeClickable(By.xpath("//*[@id='identifierId']"));
-			gmail_email.sendKeys("nishadhiman0027@gmail.com");
+			gmail_email.sendKeys("parul.pahwa@slidetech.in");
 			WebElement next_1 = driver.findElement(By.cssSelector("#identifierNext > div > button > span"));
 			next_1.click();
 
 			Thread.sleep(3000);
 			WebElement gmail_pass = wait.until(ExpectedConditions
 					.elementToBeClickable(By.cssSelector("#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input")));
-			gmail_pass.sendKeys("Qwerty@1");
+			gmail_pass.sendKeys("parulpahwa@12");
 
 			WebElement next_2 = driver.findElement(By.cssSelector("#passwordNext > div > button > span"));
 			next_2.click();
