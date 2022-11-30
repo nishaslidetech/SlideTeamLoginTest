142143aab2fee0962970aeeba759f5c8219be321
nishaslidetech <nisha.dhiman@slidetech.in>
2022-11-30 14:17:59 +0530

    update

--- src/test/java/Setupclass/TestRunner.java
+++ src/test/java/Setupclass/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "." }, glue = { "StepDefination" }, tags = ("@SLI_Search_with_facebook_Paid"),
+@CucumberOptions(features = { "." }, glue = { "StepDefination" }, //tags = ("@SLI_Search_with_facebook_Paid"),
 plugin = { "pretty",
 		"html:target/site/cucumber-pretty", "json:target/cucumber/cucumber.json", "usage:target/usage.jsonx",
 		"junit:target/cucumber.xml" })
